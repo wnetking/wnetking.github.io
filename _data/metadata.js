94976d61c4145b281fdf7819c22a3ddfe73ba9fe
wnetking <wnetking.netpeak@gmail.com>
2023-08-08 21:57:43 +0100

    Add GitHub action

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Illia Kyrylov | frontend",
+	url: "https://wnetking.github.io/",
+	language: "en",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "Illia Kyrylov",
+		email: "wnetking1@gmail.com",
+		url: "https://wnetking.github.io/"
+	}
+}
---
+++ content/blog/fifthpost.md
@@ -0,0 +1,12 @@
+---
+title: This is a fifth post (draft)
+date: 2023-01-23
+draft: true
+---
+This is a draft post
+
+```tsx
+function test() {
+
+}
+```
